5c1394f1c6463339ae247f50e7761292665d06df
KIJ <dlrwn0730@naver.com>
2025-11-27 21:45:44 +0900

    사업자 전환 로직 제거

--- user/route.js
+++ user/route.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import {
-  applyBusiness,
   updateProfile,
   changePassword,
   updateProfileImage,
@@ -11,7 +10,6 @@ const router = Router();
 
 router.use(verifyToken);
 
-router.post("/business-apply", applyBusiness);
 router.patch("/profile", updateProfile);
 router.patch("/password", changePassword);
 router.patch("/profile/image", updateProfileImage);
